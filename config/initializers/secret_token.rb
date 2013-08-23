520ce35a5b1eed2d819050a7cca3634b0d7c04ca
Anhtd <black.cat.3007@gmail.com>
2013-08-23 14:44:56 +0700

    Initalize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = 'd972fb54c66b4dc7e8bff1f0bd4fe0ff941f4948160f374fe5e38fa6c396f4704d979fb77bf9fe092d609c0324575f11db82cb5367f4700b8af5b630489ac536'
